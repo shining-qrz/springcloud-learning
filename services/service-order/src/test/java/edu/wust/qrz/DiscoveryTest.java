bb7241d06d5c1a176e13b337f40b1a980df05f33
shining <1909906595@qq.com>
2025-07-20 19:20:54 +0800

    feat: 完成NacosServiceDiscovery api单元测试，获取服务ip与端口

--- services/service-order/src/test/java/edu/wust/qrz/DiscoveryTest.java
+++ services/service-order/src/test/java/edu/wust/qrz/DiscoveryTest.java
@@ -1,5 +1,7 @@
 package edu.wust.qrz;
 
+import com.alibaba.cloud.nacos.discovery.NacosServiceDiscovery;
+import com.alibaba.nacos.api.exception.NacosException;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,12 +16,26 @@ public class DiscoveryTest {
     @Resource
     DiscoveryClient discoveryClient;
 
+    @Resource
+    NacosServiceDiscovery nacosServiceDiscovery;
+
 
     @Test
     void discoveryClientTest(){
         for (String service : discoveryClient.getServices()) {
             System.out.println("service: " + service);
             List<ServiceInstance> instances = discoveryClient.getInstances(service);
+            for (ServiceInstance instance : instances) {
+                System.out.println("ip: " + instance.getHost() + "; " + "port: " + instance.getPort());
+            }
+        }
+    }
+
+    @Test
+    void nacosServiceDiscoveryTest() throws NacosException {
+        for (String service : nacosServiceDiscovery.getServices()) {
+            System.out.println("service: " + service);
+            List<ServiceInstance> instances = nacosServiceDiscovery.getInstances(service);
             for (ServiceInstance instance : instances) {
                 System.out.println("ip: " + instance.getHost() + "; " + "port: " + instance.getPort());
             }
