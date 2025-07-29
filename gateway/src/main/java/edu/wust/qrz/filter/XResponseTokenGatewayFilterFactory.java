8e415b975f19c7d3c7948b7903aa10dccce3a733
shining <1909906595@qq.com>
2025-07-29 19:13:51 +0800

    feat: 自定义XResponseTokenGatewayFilterFactory

---
+++ gateway/src/main/java/edu/wust/qrz/filter/XResponseTokenGatewayFilterFactory.java
@@ -0,0 +1,39 @@
+package edu.wust.qrz.filter;
+
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Component
+public class XResponseTokenGatewayFilterFactory extends AbstractNameValueGatewayFilterFactory {
+    @Override
+    public GatewayFilter apply(NameValueConfig config) {
+        return new GatewayFilter() {
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+                return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                    ServerHttpResponse response = exchange.getResponse();
+                    // 在响应头中添加自定义的X-Response-Token
+                    String responseValue = "";
+                    if("uuid".equalsIgnoreCase(config.getValue()))
+                        responseValue = UUID.randomUUID().toString();
+
+                    if("jwt".equalsIgnoreCase(config.getValue()))
+                        responseValue = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
+                                "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ." +
+                                "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+                    response.getHeaders().add(config.getName(), responseValue);
+                }));
+            }
+        };
+    }
+}
--- gateway/src/main/java/edu/wust/qrz/filter/global/RTGlobalFilter.java
+++ gateway/src/main/java/edu/wust/qrz/filter/global/RTGlobalFilter.java
@@ -1,11 +1,10 @@
-package edu.wust.qrz.filter;
+package edu.wust.qrz.filter.global;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
